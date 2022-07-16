10a5110ec4d8929a919c78c5f91fc79dc13a0b5a
EECvision <eec.studies@gmail.com>
2022-07-16 03:25:54 +0100

    implement sender link

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -13,7 +13,7 @@ const auth = getAuth();
 const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: "localhost",
+  url: "https://firebase-services.vercel.app/",
   // This must be true.
   handleCodeInApp: true,
   // iOS: {
